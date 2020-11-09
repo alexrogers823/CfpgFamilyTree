89107152a9b72a2530eff7b96efb45f010198449
Alex Rogers <alex.rogers823@gmail.com>
2020-11-08 20:05:50 -0500

    created model for photos

---
+++ Models/Photo.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CfpgFamilyTree.Models {
+    public class Photo {
+        public int Id { get; set; }
+
+        [Required]
+        public string PhotoUrl { get; set; }
+    }
+}
