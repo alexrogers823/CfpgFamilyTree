2379ab088311f7c4e43bad2558b605d7e4af8795
Alex Rogers <alex.rogers823@gmail.com>
2020-11-19 20:12:05 -0500

    updated migrations to include User database

---
+++ Migrations/User/20201120010841_AddedUserContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CfpgFamilyTree.Migrations.User
+{
+    public partial class AddedUserContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,7 @@ namespace CfpgFamilyTree.Models
 {
     public class User 
     {
+        [Key]
         public int Id { get; set; }
 
         [Required]
