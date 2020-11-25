8c60646e0851a4a605afa9cfc486443a836103e9
Alex Rogers <alex.rogers823@gmail.com>
2020-11-24 18:59:56 -0600

    made migrations for consolidating all models into one database

---
+++ Migrations/Cfpg/20201125005758_CentralizeDatabase.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CfpgFamilyTree.Migrations.Cfpg
+{
+    public partial class CentralizeDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PreferredName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Suffix = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProfilePhotoUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirthDay = table.Column<int>(type: "int", maxLength: 2, nullable: false),
+                    BirthMonth = table.Column<int>(type: "int", maxLength: 2, nullable: false),
+                    BirthYear = table.Column<int>(type: "int", maxLength: 4, nullable: false),
+                    DeathDay = table.Column<int>(type: "int", maxLength: 2, nullable: false),
+                    DeathMonth = table.Column<int>(type: "int", maxLength: 2, nullable: false),
+                    DeathYear = table.Column<int>(type: "int", maxLength: 4, nullable: false),
+                    IsAlive = table.Column<bool>(type: "bit", nullable: false),
+                    HasSpouse = table.Column<bool>(type: "bit", nullable: false),
+                    PrimaryParent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondaryParent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActiveUser = table.Column<bool>(type: "bit", nullable: false),
+                    Spouse = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhotoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimelineEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Day = table.Column<int>(type: "int", nullable: true),
+                    Month = table.Column<int>(type: "int", nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Event = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimelineEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "TimelineEvents");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
