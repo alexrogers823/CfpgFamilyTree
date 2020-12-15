8adaa0c8d582543b87874426ed278404cd9b1064
Alex Rogers <alex.rogers823@gmail.com>
2020-12-15 12:21:55 -0600

    changed fields in member model

--- Dtos/MemberReadDto.cs
+++ Dtos/MemberReadDto.cs
@@ -26,10 +26,10 @@ namespace CfpgFamilyTree.Dtos
 
         public int DeathYear { get; set; }
 
-        public string PrimaryParent { get; set; }
+        public int PrimaryParentId { get; set; }
 
-        public string SecondaryParent { get; set; }
+        public int SecondaryParentId { get; set; }
 
-        public string Spouse { get; set; }
+        public int SpouseId { get; set; }
     }
 }
--- Dtos/MemberUpdateDto.cs
+++ Dtos/MemberUpdateDto.cs
@@ -35,13 +35,13 @@ namespace CfpgFamilyTree.Dtos
 
         public bool HasSpouse { get; set; }
 
-        public string PrimaryParent { get; set; }
+        public int? PrimaryParentId { get; set; }
 
-        public string SecondaryParent { get; set; }
+        public int? SecondaryParentId { get; set; }
 
         public bool IsActiveUser { get; set; }
 
-        public string Spouse { get; set; }
+        public int? SpouseId { get; set; }
 
         public int? UserId { get; set; }
     }
