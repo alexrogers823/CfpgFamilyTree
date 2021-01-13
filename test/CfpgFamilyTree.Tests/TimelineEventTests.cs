002dcc9422be95e3c113fa46c7afd7b70c9d437a
Alex Rogers <alex.rogers823@gmail.com>
2021-01-12 20:50:14 -0500

    expanded timeline event model tests and made more modular

--- test/CfpgFamilyTree.Tests/TimelineEventTests.cs
+++ test/CfpgFamilyTree.Tests/TimelineEventTests.cs
@@ -4,13 +4,12 @@ using Xunit;
 
 namespace CfpgFamilyTree.Tests
 {
-    public class TimelineEventTests
+    public class TimelineEventTests : IDisposable
     {
-        [Fact]
-        public void CanChangeEvent()
+        TimelineEvent testTimelineEvent;
+        public TimelineEventTests()
         {
-            // Arrange 
-            var testTimelineEvent = new TimelineEvent
+            testTimelineEvent = new TimelineEvent
             {
                 Day = 6,
                 Month = 1,
@@ -18,12 +17,35 @@ namespace CfpgFamilyTree.Tests
                 CreatedByUserId = 1,
                 Event = "Idiots breach the capitol"
             };
+        }
 
+        public void Dispose()
+        {
+            testTimelineEvent = null;
+        }
+
+        [Fact]
+        public void CanChangeEvent()
+        {
             // Act 
             testTimelineEvent.Event = "Ossoff wins senate race";
 
             // Assert 
             Assert.Equal("Ossoff wins senate race", testTimelineEvent.Event);
         }
-    }
+
+        [Fact]
+        public void CanChangeDate()
+        {
+            testTimelineEvent.Day = 14;
+            testTimelineEvent.Month = 2;
+            testTimelineEvent.Year = 2020;
+
+            Assert.Equal(2, testTimelineEvent.Month);
+            Assert.Equal(14, testTimelineEvent.Day);
+            Assert.Equal(2020, testTimelineEvent.Year);
+        }
+
+    // TODO: Write test(s) for testing null values 
+  }
 }
