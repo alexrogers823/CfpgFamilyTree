d5efc1bf92c517326e845072b249b4b416416159
Alex Rogers <alex.rogers823@gmail.com>
2021-02-07 15:18:26 -0500

    added ability to migrate database in production environment with context

--- src/CfpgFamilyTree/Startup.cs
+++ src/CfpgFamilyTree/Startup.cs
@@ -55,8 +55,9 @@ namespace CfpgFamilyTree
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CfpgContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
